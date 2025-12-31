62bf96c2c5b1cf8e8e65d8872d57600d6e1a5b09
Donstesh <stephenshifoko@gmail.com>
2025-12-31 11:21:05 +0300

    colors update

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   // Use const constructors for better performance
   static const Color primaryColor = Color(0xFF000000);
-  static const Color accentColor = Color(0xFFD4AF37);
+  static const Color accentColor = Color(0xFFDC3545);
   static const Color backgroundColor = Color(0xFFF8F9FA);
   static const Color surfaceColor = Colors.white;
   static const Color textPrimary = Color(0xFF212529);
